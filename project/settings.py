7a5f55814e88391becc3279fb73b44930bb5dd95
Behzod <behzodtuit@gmail.com>
2022-12-05 20:24:28 +0500

    evos

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^14cglfe2bxsdum8t2iea5wu9x2j-))rawo58=hcd%rgwy@$%$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
